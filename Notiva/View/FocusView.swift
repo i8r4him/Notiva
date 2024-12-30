82b6d27a4f02a2ffa1cb3d0c11b0c69cbe0367f9
Ibrahim Abdullah <qrrbrbpvkc@privaterelay.appleid.com>
2024-12-30 21:13:22 +0100

    TabView

---
+++ Notiva/View/FocusView.swift
@@ -0,0 +1,18 @@
+//
+//  FocusView.swift
+//  Notiva
+//
+//  Created by Ibrahim Abdullah on 30.12.24.
+//
+
+import SwiftUI
+
+struct FocusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusView()
+}
